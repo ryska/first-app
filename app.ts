636af59a2f777f685706551467df9f9b6cf1dbdc
adriannarys <adriannarys94@gmail.com>
2015-11-24 19:05:40 +0100

    all

--- app.ts
+++ app.ts
@@ -1,3 +1,104 @@
 /**
  * Created by Ada on 23.11.2015.
  */
+
+/// <reference path="typings/angular2/angular2.d.ts" />
+
+import{
+    Component,
+    NgFor,
+    View,
+    bootstrap,
+} from "angular2/angular2";
+
+class Article{
+    title:string;
+    link:string;
+    votes:number;
+
+    constructor(title,link){
+        this.title=title;
+        this.link=link;
+        this.votes=0;
+    };
+    voteUp(){
+        this.votes+=1;
+        return false;
+    };
+    voteDown(){
+        this.votes-=1;
+        return false;
+    };
+
+
+}
+
+@Component({
+    selector: 'reddit-article',
+    properties: ['article']
+})
+@View({
+    template:`
+        <article>
+            <div class="votes">{{article.votes}}</div>
+            <div class="main">
+                <h2>
+                    <a href="{{article.link}}">{{article.title}}</a>
+                </h2>
+                <ul>
+                    <li><a href (click)='article.voteUp()'>upvote</a></li>
+                    <li><a href (click)='article.voteDown()'>downvote</a></li>
+                </ul>
+            </div>
+        </article>
+    `
+})
+class RedditArticle{
+    article:Article;
+
+}
+
+
+
+@Component({
+    selector: 'reddit'
+})
+@View({
+    template: `
+        <section class="new-link">
+            <div class="control-group">
+                <div><label for="title">Title:</label></div>
+                <div><input name="title" #newtitle></div>
+            </div>
+            <div class="control-group">
+                <div><label for="link">Link:</label></div>
+                <div><input name="link" #newlink></div>
+            </div>
+
+            <button (click)="addArticle(newtitle,newlink)">Submit Link</button>
+        </section>
+
+        <reddit-article *ng-for="#article of articles" [article] = "article">
+        </reddit-article>
+    `,
+    directives: [RedditArticle,NgFor]
+})
+class RedditApp{
+    articles:array<Article>;
+
+    constructor(){
+        this.articles=[
+            new Article('ang','gr.pl'),
+            new Article('hehe', 'gr')
+        ];
+    }
+
+    addArticle(title,link){
+        this.articles.push(new Article(title.value,link.value));
+        title.value ='';
+        link.value='';
+    }
+}
+
+
+bootstrap(RedditApp);
